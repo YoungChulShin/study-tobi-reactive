ab26d9c8b0ea400dc34ee94f07cbb2806324f293
YoungChulShin <go1323@gmail.com>
2022-10-11 23:37:30 +0900

    publisher, subscriber 기본 코드 추가

---
+++ src/main/java/live/ch07/SchedulerEx.java
@@ -0,0 +1,59 @@
+package live.ch07;
+
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SchedulerEx {
+
+  public static void main(String[] args) {
+
+    Logger logger = LoggerFactory.getLogger(SchedulerEx.class);
+
+    Publisher<Integer> pub = sub -> {
+      sub.onSubscribe(new Subscription() {
+        @Override
+        public void request(long n) {
+          sub.onNext(1);
+          sub.onNext(2);
+          sub.onNext(3);
+          sub.onNext(4);
+          sub.onNext(5);
+          sub.onComplete();
+        }
+
+        @Override
+        public void cancel() {
+
+        }
+      });
+    };
+
+    pub.subscribe(new Subscriber<Integer>() {
+      @Override
+      public void onSubscribe(Subscription s) {
+        logger.debug("onSubscribe");
+        s.request(Long.MAX_VALUE);
+      }
+
+      @Override
+      public void onNext(Integer integer) {
+        logger.debug("onNext: {}", integer);
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        logger.debug("onError: {}", t.getMessage(), t);
+      }
+
+      @Override
+      public void onComplete() {
+        logger.debug("onComplete");
+      }
+    });
+  }
+
+}
