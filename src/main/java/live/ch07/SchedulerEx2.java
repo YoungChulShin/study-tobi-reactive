bbb6b46f87a1611b27b05cae91ae976ed0604d0a
YoungChulShin <go1323@gmail.com>
2022-10-12 08:40:05 +0900

    scheduler 추가

--- src/main/java/live/ch07/SchedulerEx2.java
+++ src/main/java/live/ch07/SchedulerEx2.java
@@ -1,6 +1,5 @@
 package live.ch07;
 
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.reactivestreams.Publisher;
@@ -9,21 +8,24 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SchedulerEx {
+public class SchedulerEx2 {
 
   public static void main(String[] args) {
-
     Logger logger = LoggerFactory.getLogger(SchedulerEx.class);
 
     Publisher<Integer> pub = sub -> {
       sub.onSubscribe(new Subscription() {
         @Override
         public void request(long n) {
-          logger.debug("request");
+          logger.debug("request-1");
           sub.onNext(1);
+          logger.debug("request-2");
           sub.onNext(2);
+          logger.debug("request-3");
           sub.onNext(3);
+          logger.debug("request-4");
           sub.onNext(4);
+          logger.debug("request-5");
           sub.onNext(5);
           sub.onComplete();
         }
@@ -35,18 +37,16 @@ public class SchedulerEx {
       });
     };
 
-    // pub <-> subOnPub <-> sub
+    // publisher 하는 쪽이 오래 걸린다면 이쪽을 비동기로 할 수 있다
     Publisher<Integer> subOnPub = sub -> {
-//      pub.subscribe(sub);
       ExecutorService es = Executors.newSingleThreadExecutor();
       es.execute(() -> pub.subscribe(sub));
-      es.shutdown();
     };
 
+    // 구독하는 곳에서 오래 걸린다면 비동기를 걸 수 있다
     Publisher<Integer> pubOnPub = sub -> {
       subOnPub.subscribe(new Subscriber<Integer>() {
         ExecutorService es = Executors.newSingleThreadExecutor();
-
         @Override
         public void onSubscribe(Subscription s) {
           sub.onSubscribe(s);
@@ -54,16 +54,19 @@ public class SchedulerEx {
 
         @Override
         public void onNext(Integer integer) {
-          es.execute(() -> sub.onNext(integer));
+//          sub.onNext(calculateLong(integer));
+          es.execute(() -> sub.onNext(calculateLong(integer)));
         }
 
         @Override
         public void onError(Throwable t) {
-          es.execute(() -> sub.onError(t));
+//          sub.onError(t);
+          es.execute(() ->  sub.onError(t));
         }
 
         @Override
         public void onComplete() {
+//          sub.onComplete();
           es.execute(() -> sub.onComplete());
           es.shutdown();
         }
@@ -93,9 +96,16 @@ public class SchedulerEx {
       }
     });
 
-    System.out.println("exit");
-
-    // 47:13
+    System.out.println("Exit");
   }
 
+  private static Integer calculateLong(Integer source) {
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    return source * 1;
+  }
 }
