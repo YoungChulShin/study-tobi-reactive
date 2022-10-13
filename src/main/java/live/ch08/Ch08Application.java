2fcb9d131299ae81a61c2a80e9e82df4b9db4b96
YoungChulShin <go1323@gmail.com>
2022-10-14 06:32:04 +0900

    spring + async 예제

--- src/main/java/live/ch08/Ch08Application.java
+++ src/main/java/live/ch08/Ch08Application.java
@@ -1,23 +1,29 @@
 package live.ch08;
 
+import java.util.concurrent.Future;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 public class Ch08Application {
 
   @Component
   public static class MyService {
-    public String hello() throws InterruptedException {
+    @Async
+    public Future<String> hello() throws InterruptedException {
       log.info("hello()");
-      Thread.sleep(1000);
-      return "Hello";
+      Thread.sleep(2000);
+      return new AsyncResult<>("Hello");
     }
   }
 
@@ -32,8 +38,9 @@ public class Ch08Application {
   ApplicationRunner run() {
     return args -> {
       log.info("run()");
-      String result = myService.hello();
-      log.info("exit: {} ", result);
+      Future<String> f = myService.hello();
+      log.info("exit: {} ", f.isDone());
+      log.info("result: {} ", f.get());
     };
   }
 
