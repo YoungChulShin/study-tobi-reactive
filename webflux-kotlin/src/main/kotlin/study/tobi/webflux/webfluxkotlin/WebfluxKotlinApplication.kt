d02a70ce99a08da9d2c4c3219f6401a1cd1e9f7e
YoungChulShin <go1323@gmail.com>
2022-10-17 23:47:44 +0900

    프로젝트 스캐폴드

---
+++ webflux-kotlin/src/main/kotlin/study/tobi/webflux/webfluxkotlin/WebfluxKotlinApplication.kt
@@ -0,0 +1,11 @@
+package study.tobi.webflux.webfluxkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebfluxKotlinApplication>(*args)
+}
---
+++ webflux-kotlin/src/test/kotlin/study/tobi/webflux/webfluxkotlin/WebfluxKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.tobi.webflux.webfluxkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
